876ec99afc426fdbc118c03c0712bd535269d8ff
Kendra Lyndon <kendralyndon@gmail.com>
2016-09-12 09:26:39 -0600

    comment out non-functioning code

--- modules/lists/Show.js
+++ modules/lists/Show.js
@@ -13,41 +13,41 @@ export default React.createClass({
 })
 
 var Testing = React.createClass({
-  getInitialState: function() {
-    return {data: []};
-  },
-  componentDidMount: function() {
-    $.ajax({
-      url: "http://localhost:3000/api/list/100",
-      method: 'get',
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        console.log(data);
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    })
-  },
+  // getInitialState: function() {
+  //   return {data: []};
+  // },
+  // componentDidMount: function() {
+  //   $.ajax({
+  //     url: "http://localhost:3000/api/list/100",
+  //     method: 'get',
+  //     dataType: 'json',
+  //     cache: false,
+  //     success: function(data) {
+  //       console.log(data);
+  //       this.setState({data: data});
+  //     }.bind(this),
+  //     error: function(xhr, status, err) {
+  //       console.error(this.props.url, status, err.toString());
+  //     }.bind(this)
+  //   })
+  // },
   render: function() {
     return (
       <div className="test">
         <h1>Testing</h1>
         //THIS WONT WORK BECAUSE THERE IS NO COMMENTLIST
-        <Section data={this.state.data.listItems} />
+        // <Section data={this.state.data.listItems} />
       </div>
     );
   }
 });
 
-var Section = React.createClass({
-  render: function() {
-    return (
-      <div className="testingTesting">
-        <h1>{this.}</h1>
-      </div>
-    );
-  }
-});
+// var Section = React.createClass({
+//   render: function() {
+//     return (
+//       <div className="testingTesting">
+//         <h1>{this.}</h1>
+//       </div>
+//     );
+//   }
+// });
