4db40094593daf56e2cbe0bcb33e8ff4fe0639da
Kendra Lyndon <kendralyndon@gmail.com>
2016-09-12 09:40:13 -0600

    more stuff

--- modules/lists/Show.js
+++ modules/lists/Show.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import GetAllLists from '../ajax-helpers/lists/GetAllUserLists'
+import GetOneList from '../ajax-helpers/lists/GetOneList'
 
+GetAllLists();
 
 export default React.createClass({
   render() {
@@ -13,35 +15,24 @@ export default React.createClass({
 })
 
 var Testing = React.createClass({
-  // getInitialState: function() {
-  //   return {data: []};
-  // },
-  // componentDidMount: function() {
-  //   $.ajax({
-  //     url: "http://localhost:3000/api/list/100",
-  //     method: 'get',
-  //     dataType: 'json',
-  //     cache: false,
-  //     success: function(data) {
-  //       console.log(data);
-  //       this.setState({data: data});
-  //     }.bind(this),
-  //     error: function(xhr, status, err) {
-  //       console.error(this.props.url, status, err.toString());
-  //     }.bind(this)
-  //   })
-  // },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    GetOneList();
+  },
   render: function() {
     return (
       <div className="test">
         <h1>Testing</h1>
-        //THIS WONT WORK BECAUSE THERE IS NO COMMENTLIST
+        //THIS WONT WORK YET
         // <Section data={this.state.data.listItems} />
       </div>
     );
   }
 });
 
+//THIS ALSO DOESNT WORK YET
 // var Section = React.createClass({
 //   render: function() {
 //     return (
